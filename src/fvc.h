26ddd45712cb1eb0aecac3b70047e217c86de48d
Jayesh <jayeshgolhar7@gmail.com>
2022-07-20 12:09:50 +0100

    fvc namespace first steps

---
+++ src/fvc.h
@@ -0,0 +1,14 @@
+#ifndef FVC_H
+#define FVC_H
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+//#include "fvcDdt.H"
+//#include "fvcD2dt2.H"
+//#include "fvcDiv.H"
+#include "fvcLaplacian.h"
+//#include "fvcSup.H"
+
+// * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * //
+
+#endif

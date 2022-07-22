814b455215a534ab70c261f2b36bb809f1a1ea7b
wagnergaluppo <wagnergaluppo@gmail.com>
2022-07-22 22:21:37 +0100

    fvc::laplacian First try only for Boundary

--- src/fvcLaplacianI.h
+++ src/fvcLaplacianI.h
@@ -1,6 +1,6 @@
 #include "fvcLaplacian.h"
 #include "mathOperations.h"
-//#include "fvBoundaryConditionsField.h"
+#include "fvBoundaryConditionsField.h"
 
 //#include "Mesh.h"
 //#include "Face.h"
@@ -12,9 +12,9 @@ namespace fvc
     //Scalar Field situation
     std::vector<double> laplacian(const double& K, VolField<scalarField>& vf)
     {
-      std::vector<double> vecB(0);
+      std::vector<double> vecB(vf.mesh().nCells_,0.0);
       //   FVMatrix fvMatrix(vf);
-      //   //We will acess the  const Mesh& mesh attending the VolField <scalarField>& vf
+      //   //We will access the  const Mesh& mesh attending the VolField <scalarField>& vf
 
       //   //***************Internal Faces contributions**************************************************//
       //            //vf.mesh() gives the mesh of vf
@@ -40,8 +40,45 @@ namespace fvc
 
       //   };
 
-      //   // *** Boundary faces contributions  : Wagner will take care---------------------------------------------------- ***//
-    
+      // *** Boundary faces contributions  : Wagner will take care---------------------------------------------------- ***//
+      
+      // *** Boundary faces contributions ---------------------------------------------------- ***//
+
+        // Initializing boundary conditions coefficient contributions
+        fvBoundaryConditionsField<scalarField> boundaryConditions(vf);
+        long unsigned int patchesSize = vf.mesh().nPatches_;
+        double diffusionK(K);
+        for ( long unsigned int patchI = 0; patchI < patchesSize; patchI++)
+        { 
+          // std::cout << "This is the patch: " << patchI << std::endl;
+          // Loading the faces controls for this patch
+          int sizeOfPatch( boundaryConditions.coefficientsData().at(patchI).gradientInternalCoeffs.size() );
+          int startPatchFaceID = vf.mesh().patchList_[patchI].startFace();
+
+          // Looping in all the faces for this patch
+          for (int faceI = 0; faceI < sizeOfPatch; faceI++)
+          {
+            vector3 areaVec = vf.mesh().faceList_[startPatchFaceID+faceI].getAreaVector();
+            double  areaMag = mag(areaVec);
+
+            //std::cout << "This is the face: " << startPatchFaceID+faceI << std::endl;
+            // Retrieving the boundary face owner cell ID
+            int ownInd = vf.mesh().faceList_[startPatchFaceID+faceI].getOwner()->ID_;
+            
+            // std::cout << "It's Owner Cell is the cell: " << ownInd << std::endl;
+            // Loading the values to added for the laplacian term
+            double valueToAddDiagonal( boundaryConditions.coefficientsData().at(patchI).gradientInternalCoeffs.at(faceI));
+            double valueToAddSource( boundaryConditions.coefficientsData().at(patchI).gradientBoundaryCoeffs.at(faceI));
+            
+            // Adding the loaded values to the proper places
+            // fvMatrix.aMatrix_->addValue(ownInd,ownInd,(+diffusionK*valueToAddDiagonal*areaMag));
+            // fvMatrix.bVector_.at(ownInd) += (-diffusionK*valueToAddSource*areaMag) ;
+            vecB.at(ownInd) += ( +diffusionK*valueToAddDiagonal*areaMag*vf.internalFieldRef().at(ownInd) );
+            vecB.at(ownInd) += ( +diffusionK*valueToAddSource  *areaMag                                    );
+          }
+        }
+        // ***-End-of-Boundary-faces-contributions---------------------------------------------- ***// 
+
       return vecB;
     }
     
